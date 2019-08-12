f76a5370a579137f368ac0af1d180e9731ea8a80
archieren <renjp_r@hotmail.com>
2019-08-13 00:59:21 +0800

    Nothing Important!

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,14 @@
 ;; Don't forget putting (add-hook 'hack-local-variables-hook (lambda () (editorconfig-apply))) to your .emacs.
 ((c++-mode  . ((flycheck-gcc-include-path . ("/home/archie/Projects/SSD_Tracker/include/"
                                              "/usr/include/opencv4/"
-                                             "/usr/include/tensorflow/"))
+                                             "/usr/include/tensorflow/"
+                                             "/usr/include/tensorflow/bazel-out/k8-opt/bin/"))
                ))
 
  (c-mode  . ((flycheck-gcc-include-path . ("/home/archie/Projects/SSD_Tracker/include/"
                                            "/usr/include/opencv4/"
-                                           "/usr/include/tensorflow/"))
+                                           "/usr/include/tensorflow/"
+                                           "/usr/include/tensorflow/bazel-out/k8-opt/bin/"))
              ))
  )
 
